8cb7f94ede14377402a0b3be2dbbb3931dde3349
Vulp0 <mauriciomartinezvaz@gmail.com>
2023-08-09 17:31:58 -0600

    first public release i guess, this is v1.0

--- zBot.js
+++ zBot.js
@@ -29,31 +29,31 @@ bot.command("rng", ctx => {
         let result = "Here's a random number between 0 and " + userMsgArray[1] + ": \n" + (Math.floor(Math.random()*userParam)).toString();
         ctx.sendMessage(result);
     } else {
-        ctx.sendMessage("You didn't follow proper syntax, use: /rng <whole_number>");
+        ctx.sendMessage("You didn't follow proper syntax, use: /rng 100");
     }
 });
 
-bot.command("whoyou", ctx => {
-    ctx.sendMessage("I'm " + ctx.botInfo.first_name + "!");
-});
+// bot.command("whoyou", ctx => {
+//     ctx.sendMessage("I'm " + ctx.botInfo.first_name + "!");
+// });
 
-bot.command("whome", ctx => {
-    ctx.sendMessage("You're " + ctx.update.message.from.username + "!");
-});
+// bot.command("whome", ctx => {
+//     ctx.sendMessage("You're " + ctx.update.message.from.username + "!");
+// });
 
 //note to myself, telegram is not like discord so if you want to handle arguments in commands, you'll have to do something like this
 //separate the user's text into an array, where index 0 will be the /command itself, and from index 1 onwards are the "parameters" for the command
-bot.command("say", ctx => {
-    let userMsgArray = ctx.message.text.split(" ");
+// bot.command("say", ctx => {
+//     let userMsgArray = ctx.message.text.split(" ");
 
-    if(userMsgArray.length == 1){
-        ctx.sendMessage("Say what? Use: /say <whatever>");
-    } else {
-        userMsgArray.shift();
-        let userMessage = userMsgArray.join(" ");
-        ctx.sendMessage(userMessage);
-    }
-});
+//     if(userMsgArray.length == 1){
+//         ctx.sendMessage("Say what? Use: /say <whatever>");
+//     } else {
+//         userMsgArray.shift();
+//         let userMessage = userMsgArray.join(" ");
+//         ctx.sendMessage(userMessage);
+//     }
+// });
 
 //test command, no practical purpose
 bot.command("shibe", ctx => {
@@ -66,15 +66,15 @@ bot.command("shibe", ctx => {
 });
 
 //test command, serves no practical purpose other than me learning how it works
-bot.command("replyme", ctx => {
-    let userMsgArray = ctx.message.text.split(" ");
+// bot.command("replyme", ctx => {
+//     let userMsgArray = ctx.message.text.split(" ");
 
-    let messageTemplate = "Replying to <b>" + ctx.message.from.first_name + "'s</b> message. Message content: \"<i>" + userMsgArray.join(" ") + "</i>\"";
-    ctx.reply(messageTemplate, {
-        reply_to_message_id: ctx.message.message_id,
-        parse_mode: "HTML"
-    });
-});
+//     let messageTemplate = "Replying to <b>" + ctx.message.from.first_name + "'s</b> message. Message content: \"<i>" + userMsgArray.join(" ") + "</i>\"";
+//     ctx.reply(messageTemplate, {
+//         reply_to_message_id: ctx.message.message_id,
+//         parse_mode: "HTML"
+//     });
+// });
 
 // bot.command("sma", ctx => {
 //     //command use should be: /sma <symb> <timeWindow>
@@ -127,47 +127,47 @@ bot.command("credits", ctx => {
     });
 });
 
-bot.command("in", ctx => {
-    let userMsgArray = ctx.message.text.split(" ");
-    let messageTemplate = "";
-    let userInput = parseFloat(userMsgArray[1]);
+// bot.command("in", ctx => {
+//     let userMsgArray = ctx.message.text.split(" ");
+//     let messageTemplate = "";
+//     let userInput = parseFloat(userMsgArray[1]);
 
-    if(userMsgArray.length == 1) {
-        ctx.sendMessage("Command incomplete, correct command usage: \"/in 10\" ");
-    } else {
-        if(isNaN(userInput)) {
-            ctx.sendMessage(`That's not a valid number, try again`);
-        } else {
-            userInput == 1 ? messageTemplate += `${userInput} inch is equal to:` : messageTemplate += `${userInput} inches are equal to:`
-            messageTemplate += `\n• ${userInput * 2.54 } centimeters, \n• ${userInput * 0.0254 } meters`;
+//     if(userMsgArray.length == 1) {
+//         ctx.sendMessage("Command incomplete, correct command usage: \"/in 10\" ");
+//     } else {
+//         if(isNaN(userInput)) {
+//             ctx.sendMessage(`That's not a valid number, try again`);
+//         } else {
+//             userInput == 1 ? messageTemplate += `${userInput} inch is equal to:` : messageTemplate += `${userInput} inches are equal to:`
+//             messageTemplate += `\n• ${userInput * 2.54 } centimeters, \n• ${userInput * 0.0254 } meters`;
     
-            ctx.sendMessage(messageTemplate, {
-                parse_mode: "HTML"
-            });
-        }
-    }
-});
+//             ctx.sendMessage(messageTemplate, {
+//                 parse_mode: "HTML"
+//             });
+//         }
+//     }
+// });
 
-bot.command("ft", ctx => {
-    let userMsgArray = ctx.message.text.split(" ");
-    let messageTemplate = "";
-    let userInput = parseFloat(userMsgArray[1]);
+// bot.command("ft", ctx => {
+//     let userMsgArray = ctx.message.text.split(" ");
+//     let messageTemplate = "";
+//     let userInput = parseFloat(userMsgArray[1]);
 
-    if(userMsgArray.length == 1) {
-        ctx.sendMessage("Command incomplete, Correct command usage: \"/in 10\" ");
-    } else {
-        if(isNaN(userInput)) {
-            ctx.sendMessage(`That's not a valid number, try again`);
-        } else {
-            userInput == 1 ? messageTemplate += `${userInput} inch is equal to:` : messageTemplate += `${userInput} inches are equal to:`
-            messageTemplate += `\n• ${userInput * 30.48 } centimeters, \n• ${userInput * 0.3048 } meters, \n• ${userInput * 12 } inches`;
+//     if(userMsgArray.length == 1) {
+//         ctx.sendMessage("Command incomplete, Correct command usage: \"/in 10\" ");
+//     } else {
+//         if(isNaN(userInput)) {
+//             ctx.sendMessage(`That's not a valid number, try again`);
+//         } else {
+//             userInput == 1 ? messageTemplate += `${userInput} inch is equal to:` : messageTemplate += `${userInput} inches are equal to:`
+//             messageTemplate += `\n• ${userInput * 30.48 } centimeters, \n• ${userInput * 0.3048 } meters, \n• ${userInput * 12 } inches`;
     
-            ctx.sendMessage(messageTemplate, {
-                parse_mode: "HTML"
-            });
-        }
-    }
-});
+//             ctx.sendMessage(messageTemplate, {
+//                 parse_mode: "HTML"
+//             });
+//         }
+//     }
+// });
 
 bot.command("check", ctx => {
     let userMsgArray = ctx.message.text.split(" ");
@@ -190,27 +190,23 @@ bot.command("check", ctx => {
             }),
             body: JSON.stringify({
                 currency: "USD",
-                code: userInput,
+                code: userInput.toUpperCase(),
                 meta: true
             })
         })
         .then(response => response.json())
         .then(jsonResponse => {
             let result = jsonResponse;
-            
-            if("error" in result) {
-                ctx.sendMessage("Error, coin code is either unknown or not in all caps. \nCorrect command usage: /check BTC");
-            } else {
-                //okay so, i can't directly use sendMessage(result) because it says there's no text, but i can send each 
-                //this is so janky oh my god
-                let messageTemplate = `Coin: ${result["name"]}\nRanked: ${result["rank"]}\nTraded in ${result["exchanges"]} exchanges.\n\nWith a total supply of ${result["totalSupply"]} out of ${result["maxSupply"]}, and a circulating supply of ${result["circulatingSupply"]}.\n\nCurrent price as of (right now): $${result["rate"]}\nVolume: ${result["volume"]}\nMarket cap: ${result["cap"]}
-                `;
-
-                ctx.sendMessage(messageTemplate);
-            }
+            //this is such a mess, man
+            let messageTemplate = `${result["name"]} (${userInput.toUpperCase()})\n$Priced at $${result["rate"]}\nAs of: ${new Date}`;
+            ctx.sendMessage(messageTemplate);
         });
     }
 
 });
+
+bot.command("emo", ctx => {
+    ctx.sendMessage("e");
+})
 
 bot.launch();
