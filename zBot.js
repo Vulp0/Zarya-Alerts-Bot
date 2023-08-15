8e31c9e754996528d48a583eeb71e21be8e6f1d6
Vulp0 <mauriciomartinezvaz@gmail.com>
2023-08-14 19:19:49 -0600

    removed a few useless things

--- zBot.js
+++ zBot.js
@@ -10,9 +10,6 @@ bot.start(ctx => {
     ctx.sendMessage("You've just started this bot, how cool is that?");
 });
 
-//start, help and settings all have to be defined commands, because of good practices
-//idk what to put in them yet so they're commented out for now
-
 bot.help(ctx => {
 
     let userMsgArray = ctx.message.text.split(" ");
@@ -24,7 +21,7 @@ bot.help(ctx => {
         userArg = userMsgArray[1];
 
         switch (userArg) {
-            //prepare for JANK JANK JANK JANKSJKA
+            //prepare for JANK
             case commandList[0]:
                 messageTemplate += "<b>Command:</b> /credits" + "\n<b>Parameters:</b> <i>NONE</i>";
                 messageTemplate += "\n<b>Desc:</b> Shows you how many Livecoinwatch api calls you have left." + "\n<b>Example:</b> /credits";
@@ -70,12 +67,6 @@ bot.help(ctx => {
             parse_mode: "HTML"
         });
     }
-    //later...
-    // messageTemplate += "<b>Command:</b> /check" + "\n<b>Usage example:</b> /check <i>code</i>";
-    // messageTemplate += "\n<b>Description:</b> Given a coin code, returns the name, price and the date and time of the request.";
-    // ctx.sendMessage(messageTemplate, {
-    //     parse_mode: "HTML"
-    // });
 });
 
 // bot.settings(ctx => {
@@ -94,29 +85,6 @@ bot.command("rng", ctx => {
     }
 });
 
-// bot.command("whoyou", ctx => {
-//     ctx.sendMessage("I'm " + ctx.botInfo.first_name + "!");
-// });
-
-// bot.command("whome", ctx => {
-//     ctx.sendMessage("You're " + ctx.update.message.from.username + "!");
-// });
-
-//note to myself, telegram is not like discord so if you want to handle arguments in commands, you'll have to do something like this
-//separate the user's text into an array, where index 0 will be the /command itself, and from index 1 onwards are the "parameters" for the command
-// bot.command("say", ctx => {
-//     let userMsgArray = ctx.message.text.split(" ");
-
-//     if(userMsgArray.length == 1){
-//         ctx.sendMessage("Say what? Use: /say <whatever>");
-//     } else {
-//         userMsgArray.shift();
-//         let userMessage = userMsgArray.join(" ");
-//         ctx.sendMessage(userMessage);
-//     }
-// });
-
-//test command, no practical purpose
 bot.command("shibe", ctx => {
     ctx.sendMessage("Getting a random shiba picture, wait a moment");
     fetch("https://shibe.online/api/shibes?count=1&urls=true&httpsUrls=true")
@@ -137,38 +105,6 @@ bot.command("shibe", ctx => {
 //     });
 // });
 
-// bot.command("sma", ctx => {
-//     //command use should be: /sma <symb> <timeWindow>
-//     //where symb(type: str, required) is the symbol of the given coin
-//     //where timeWindow(type: int, required) is how many days it should look back in time
-//     let userMsgArray = ctx.message.text.split(" ");
-//     // let symb = userMsgArray[1];
-//     // let timeWindow = userMsgArray[2];
-//     let apiCallUrl = `https://api.polygon.io/v1/indicators/sma/X:BTCUSD?timespan=day&window=1&series_type=close&order=desc&limit=10&apiKey=${process.env.PKEY}`
-
-//     //test call, to see if api key works
-//     fetch(apiCallUrl)
-//         .then(response => response.json())
-//         .then(jsonResponse => {
-//             let result = jsonResponse;
-            
-//             //to access anything in result, use bracket notation like ["results"] instead of dot notation like .results 
-//             //this will print the timestamp and value of the first result of the call
-//             // console.log(result["results"]["values"][0]["timestamp"]);
-//             // console.log(result["results"]["values"][0]["value"]);
-
-//             //shows the amount of values
-//             // console.log(result["results"]["values"].length);
-//         });
-
-
-// });
-
-// bot.command("alist", ctx => {
-//     //command use: /alist <limit> <sort> <order> <meta>
-//     //where
-// })
-
 bot.command("credits", ctx => {
     let apiCallUrl = "https://api.livecoinwatch.com/credits";
 
@@ -187,48 +123,6 @@ bot.command("credits", ctx => {
         ctx.sendMessage(messageTemplate);
     });
 });
-
-// bot.command("in", ctx => {
-//     let userMsgArray = ctx.message.text.split(" ");
-//     let messageTemplate = "";
-//     let userInput = parseFloat(userMsgArray[1]);
-
-//     if(userMsgArray.length == 1) {
-//         ctx.sendMessage("Command incomplete, correct command usage: \"/in 10\" ");
-//     } else {
-//         if(isNaN(userInput)) {
-//             ctx.sendMessage(`That's not a valid number, try again`);
-//         } else {
-//             userInput == 1 ? messageTemplate += `${userInput} inch is equal to:` : messageTemplate += `${userInput} inches are equal to:`
-//             messageTemplate += `\n• ${userInput * 2.54 } centimeters, \n• ${userInput * 0.0254 } meters`;
-    
-//             ctx.sendMessage(messageTemplate, {
-//                 parse_mode: "HTML"
-//             });
-//         }
-//     }
-// });
-
-// bot.command("ft", ctx => {
-//     let userMsgArray = ctx.message.text.split(" ");
-//     let messageTemplate = "";
-//     let userInput = parseFloat(userMsgArray[1]);
-
-//     if(userMsgArray.length == 1) {
-//         ctx.sendMessage("Command incomplete, Correct command usage: \"/in 10\" ");
-//     } else {
-//         if(isNaN(userInput)) {
-//             ctx.sendMessage(`That's not a valid number, try again`);
-//         } else {
-//             userInput == 1 ? messageTemplate += `${userInput} inch is equal to:` : messageTemplate += `${userInput} inches are equal to:`
-//             messageTemplate += `\n• ${userInput * 30.48 } centimeters, \n• ${userInput * 0.3048 } meters, \n• ${userInput * 12 } inches`;
-    
-//             ctx.sendMessage(messageTemplate, {
-//                 parse_mode: "HTML"
-//             });
-//         }
-//     }
-// });
 
 bot.command("check", ctx => {
     let userMsgArray = ctx.message.text.split(" ");
